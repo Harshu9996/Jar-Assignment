ee7657e9c10a35769da4a7bc212bbde7ce1476e3
Harshu9996 <mharshsharma211@gmail.com>
2024-12-24 11:33:30 +0530

    Removed transparent color of Text composable, added emit function in JarRepositoryImpl, added collectLatest function to collect flow data from fetchresult function in JarViewModel and updating the _listStringData state flow

--- app/src/main/java/com/myjar/jarassignment/ui/vm/JarViewModel.kt
+++ app/src/main/java/com/myjar/jarassignment/ui/vm/JarViewModel.kt
@@ -1,13 +1,18 @@
 package com.myjar.jarassignment.ui.vm
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.myjar.jarassignment.createRetrofit
 import com.myjar.jarassignment.data.model.ComputerItem
 import com.myjar.jarassignment.data.repository.JarRepository
 import com.myjar.jarassignment.data.repository.JarRepositoryImpl
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class JarViewModel : ViewModel() {
@@ -16,11 +21,18 @@ class JarViewModel : ViewModel() {
     val listStringData: StateFlow<List<ComputerItem>>
         get() = _listStringData
 
+
+
     private val repository: JarRepository = JarRepositoryImpl(createRetrofit())
 
     fun fetchData() {
         viewModelScope.launch {
-            repository.fetchResults()
+            repository.fetchResults().collectLatest {computerItemList->
+                _listStringData.update {
+                    computerItemList
+                }
+
+            }
         }
     }
 }
